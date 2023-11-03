13b8fbdba6595441ac66bf5aa7a9cd131b22bb2a
Jasur <ibraimov0409@mail.ru>
2023-11-03 21:57:58 +0600

    Added Prisma

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Link from "next/link"
 
 function Logo() {
   return (
-    <div>Logo</div>
+    <Link href={"/"} className='font-bold text-3xl bg-gradient-to-r from-orange-400 to-red-400 text-transparent bg-clip-text hover:cursor-pointer'>FormBuilder</Link>
   )
 }
 
